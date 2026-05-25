0837149752860f4451dfa798ad6f6e34279f7e00
gznnk <32328338+gznnk@users.noreply.github.com>
2026-05-25 18:54:23 +0900

    test: カバレッジ設定を調整

--- packages/geometry/vitest.config.ts
+++ packages/geometry/vitest.config.ts
@@ -5,5 +5,8 @@ export default defineConfig({
 		globals: true,
 		environment: "node",
 		include: ["src/**/__tests__/**/*.{test,spec}.{ts,tsx}"],
+		coverage: {
+			exclude: ["src/**/index.ts", "vitest.config.ts"],
+		},
 	},
 });
